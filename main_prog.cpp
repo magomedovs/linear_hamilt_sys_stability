c03ed6e8d8de1159d382fd4c6573b92e36d342ff
magomedovs <shamil96@mail.ru>
2022-03-28 18:51:49 +0300

    eigenvalues

--- main_prog.cpp
+++ main_prog.cpp
@@ -1,3 +1,6 @@
+#include <Eigen/Dense>
+#include <Eigen/Eigenvalues> // ?
+
 #include "linspace.h"
 #include "jacobi_am_from_boost.h"
 #include "ode_system_classes.h"
@@ -7,15 +10,16 @@
 #include <iostream>
 #include <fstream>
 #include <array>
+#include <complex>
 
 int main()
 {
 /*	test for monodromy matrix */
-//	const Oscillation_system obj(0.5, 0.65, -0.5);
-//	const size_t DIM(obj.DIM);
-//	typedef std::array<double, DIM> state_type;
-//
-//	std::array<state_type, DIM> monodr_mat = Monodromy_matrix<Oscillation_system, DIM>(obj);
+	const Oscillation_system obj(0.5, 0.65, 0.5);
+	const size_t DIM(obj.DIM);
+	typedef std::array<double, DIM> state_type;
+
+	std::array<state_type, DIM> monodr_mat = Monodromy_matrix<Oscillation_system, DIM>(obj);
 //	for (size_t i = 0; i < DIM; ++i) {
 //		for (size_t j = 0; j < DIM; ++j) {
 //			std::cout << monodr_mat[i][j] << " ";
@@ -23,21 +27,31 @@ int main()
 //		std::cout << "\n";
 //	}
 
+	Eigen::Matrix<double, DIM, DIM> eig_mat;
+	for (size_t i = 0; i < DIM; ++i) {
+		for (size_t j = 0; j < DIM; ++j) {
+			eig_mat(i, j) = monodr_mat[i][j];
+		}
+	}
+	Eigen::Matrix<std::complex<double>, DIM, 1> eigenvals( eig_mat.eigenvalues() );
+	std::cout << eigenvals << "\n";
+	std::cout << "abs values: " << std::abs(eigenvals(0)) << " " << std::abs(eigenvals(1)) << "\n";
+
 /*	test for ode integration */
 //	Oscillation_system Rotation_system
-	typedef std::array<double, Base_system::DIM> state_type;
-	const Rotation_system obj(0.5, 0.85, 1.5);
+//	typedef std::array<double, Base_system::DIM> state_type;
+//	const Rotation_system obj(0.5, 0.85, 1.5);
 //	const Oscillation_system obj(0.5, 0.65, -0.5);
-	state_type x;
-	x[0] = 1.;
-	x[1] = 0.;
-	const double t1 = 0;
-	const double t2 = 20. * obj.T;
-	const double dt = 0.0001;
-
-	IntegrateSystem(obj, x, t1, t2, dt, Integrator_Type::dopri5, true);
-
-	std::cout << x[0] << " " << x[1] << "\n";
+//	state_type x;
+//	x[0] = 1.;
+//	x[1] = 0.;
+//	const double t1 = 0;
+//	const double t2 = 1. * obj.T;
+//	const double dt = 0.0001;
+//
+//	IntegrateSystem(obj, x, t1, t2, dt, Integrator_Type::dopri5, true);
+//
+//	std::cout << x[0] << " " << x[1] << "\n";
 
 	return 0;
 }
